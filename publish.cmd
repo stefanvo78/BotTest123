2cd3358bbea6753662e3e32c83099d798fa7df88
Stefan Volkmer <s.volkmer@orange-networks.de>
2018-04-09 19:27:37 +0200

    initial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=testbotabc123-Web-Deploy.pubxml -p:Password=cfLYW7CwCqYwzZTm20dyKQnwheSPpqMneHXSAFxuq9SmqWBXtmvhcNvuuaR0
+
